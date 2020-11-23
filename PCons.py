3a28afe6adfda2510f94ad564df871e5b8fb6852
Daniel Castillo <dacastillo@pop-os.localdomain>
2020-11-23 19:08:48 -0300

    Hecho con Script

---
+++ PCons.py
@@ -0,0 +1,16 @@
+def Cons(Ser Humano) 
+       if(...):
+              Ser Humano Es Chileno
+              Derechos y Responsabilidades de la Población Chilena
+              if(...):
+                  Chileno es Ciudadano
+                  Derechos y Responsabilidades de los Ciudadanos Chilenos
+                  if(...):
+                     Ciudadano es Miembro de Organización Civil
+                     Derechos y Responsabilidades de las Organizaciones Civiles
+                  if(...):
+                     Ciudadano es elegido para Cargo de Representación Popular
+                     Derechos y Responsabilidades de los Cargos de representación popular
+                   if(...):
+                     Ciudadano es sorteado para Cargo de Consulta y Asesoría
+                     Derechos y Responsabilidades de los Cargos de Consulta y Asesoría
